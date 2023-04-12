c9c951a9c1131638c533fb9de7f49ac1e0fd10c0
Blazej_Knie <bjke@gft.com>
2023-04-12 14:43:41 +0200

    change parent pom, datasource username, password and disable one inventoryServiceRestTemplateImplTest

--- src/test/java/com/blazejknie/msscbeerservice/services/inventory/InventoryServiceRestTemplateImplTest.java
+++ src/test/java/com/blazejknie/msscbeerservice/services/inventory/InventoryServiceRestTemplateImplTest.java
@@ -1,10 +1,16 @@
 package com.blazejknie.msscbeerservice.services.inventory;
 
 import com.blazejknie.msscbeerservice.bootstrap.BeerLoader;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@Disabled
 @SpringBootTest
 class InventoryServiceRestTemplateImplTest {
 
@@ -15,5 +21,7 @@ class InventoryServiceRestTemplateImplTest {
     void testGetOnhandInventory() {
         Integer onhandInventory = inventoryService.getOnhandInventory(BeerLoader.BEER_1_UUID);
         System.out.println(onhandInventory);
+
+        assertFalse(Objects.isNull(onhandInventory));
     }
 }
